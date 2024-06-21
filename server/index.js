024f512112994d8e51a2c630851d832516a925a2
Asish Jose <asishjosekakkattil@gmail.com>
2024-06-21 15:59:47 +0530

    working

--- server/index.js
+++ server/index.js
@@ -14,8 +14,6 @@ import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
-// import User from "./models/User.js";
-// import Post from "./models/Post.js";
 // import { users, posts } from "./data/index.js";
 
 /* CONFIGURATIONS */
@@ -58,8 +56,6 @@ mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
-
-    /* ADD DATA ONE TIME */
     // User.insertMany(users);
     // Post.insertMany(posts);
   })
